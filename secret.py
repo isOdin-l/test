33b50db2a8d2c485c0c3a2fba0f5f43f17f85aa8
isOdin-l <ivanbildagin@mail.ru>
2026-02-03 15:03:25 +0300

    added secrets

---
+++ secret.py
@@ -0,0 +1,12 @@
+# AWS keys
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# GitHub Personal Access Token
+GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz123456789"
+
+# Slack webhook
+SLACK_WEBHOOK = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+
+# Hardcoded password
+DB_PASSWORD = "supersecretpassword123"
