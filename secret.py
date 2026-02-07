b476f207e7f29f105712f20759e87fbdc72d2ce0
isOdin-l <ivanbildagin@mail.ru>
2026-02-07 20:01:57 +0000

    Autofix by SWE Agent 2026-02-07 20:01:57

--- secret.py
+++ secret.py
@@ -1,3 +1,4 @@
+import os
 # AWS keys
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
@@ -6,7 +7,7 @@ AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz123456789"
 
 # Slack webhook
-SLACK_WEBHOOK = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+SLACK_WEBHOOK = os.environ.get("SLACK_WEBHOOK")
 
 # Hardcoded password
 DB_PASSWORD = "supersecretpassword123"
